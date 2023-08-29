3266aef0786c85db43835026e58d764ad3367dc0
lunaf-github <lunaf91@gmail.com>
2023-08-28 19:04:47 -0700

    Set up graphql

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
-const PORT = 5000;
+const PORT = 4000;
+
+const schema = buildSchema(
+  `
+  type Query {
+    hello: String
+  }
+  `
+);
+
+
+const root = {
+  hello: () => {
+   return "Hello world!"
+  },
+}
+
+
+app.use('/graphql', graphqlHTTP({
+ schema: schema,
+ rootValue: root,
+ graphiql: true
+}));
+
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
